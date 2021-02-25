19fd3ed0b90dfa8cdc8e22c0ef60ce22ac9cf68e
Hendrik Mans <hendrik@mans.de>
2021-02-25 11:11:00 +0100

    Remove scaffolding, add GameLive

---
+++ lib/space_rage_web/live/game_live.ex
@@ -0,0 +1,3 @@
+defmodule SpaceRageWeb.GameLive do
+  use SpaceRageWeb, :live_view
+end
--- lib/space_rage_web/router.ex
+++ lib/space_rage_web/router.ex
@@ -17,7 +17,7 @@ defmodule SpaceRageWeb.Router do
   scope "/", SpaceRageWeb do
     pipe_through :browser
 
-    live "/", PageLive, :index
+    live "/", GameLive, :index
   end
 
   # Other scopes may use custom stacks.
