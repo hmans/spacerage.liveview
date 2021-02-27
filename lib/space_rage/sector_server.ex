f6e5b822ff758b22debc221994ffa2adc8457f5d
Hendrik Mans <hendrik@mans.de>
2021-02-27 12:27:29 +0100

    Subscribe SectorServer to pubsub, too

--- lib/space_rage/sector_server.ex
+++ lib/space_rage/sector_server.ex
@@ -4,7 +4,16 @@ defmodule SpaceRage.SectorServer do
   def start_link(sector_id),
     do: GenServer.start_link(__MODULE__, sector_id, name: :"sector:#{sector_id}")
 
+  @impl true
   def init(_sector_id) do
+    # Subscribe this process to pubsub
+    Phoenix.PubSub.subscribe(SpaceRage.PubSub, "sector:1")
+
     {:ok, nil}
+  end
+
+  @impl true
+  def handle_info({:new_player}, state) do
+    {:noreply, state}
   end
 end
--- lib/space_rage_web/live/game_live.ex
+++ lib/space_rage_web/live/game_live.ex
@@ -1,8 +1,26 @@
 defmodule SpaceRageWeb.GameLive do
   use SpaceRageWeb, :live_view
 
+  alias Phoenix.PubSub
+
   @impl true
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      # Subscribe to PubSub events for this sector
+      PubSub.subscribe(
+        SpaceRage.PubSub,
+        "sector:1"
+      )
+
+      # Notify pubsub about the arrival of this player
+      PubSub.broadcast(SpaceRage.PubSub, "sector:1", {:new_player})
+    end
+
     {:ok, socket}
+  end
+
+  @impl true
+  def handle_info({:new_player}, socket) do
+    {:noreply, socket}
   end
 end
